c388a3344b85d462f7866d3c080bcdb18ad32b38
axiomproject <johncharlesvergara01@gmail.com>
2025-02-19 01:43:42 +0800

    hehe

--- frontend/src/types/react-icons.d.ts
+++ frontend/src/types/react-icons.d.ts
@@ -1,7 +1,34 @@
 declare module 'react-icons/fa' {
-  export * from '@types/react-icons/fa';
+  import { IconType } from 'react-icons';
+  
+  export const FaMapMarkerAlt: IconType;
+  export const FaCalendarAlt: IconType;
+  export const FaClock: IconType;
+  export const FaPhoneAlt: IconType;
+  export const FaEnvelope: IconType;
 }
 
 declare module 'react-icons/fi' {
-  export * from '@types/react-icons/fi';
+  import { IconType } from 'react-icons';
+  
+  export const FiEdit: IconType;
+  export const FiUpload: IconType;
+  export const FiMapPin: IconType;
+  export const FiMessageSquare: IconType;
+  export const FiCheck: IconType;
+  export const FiX: IconType;
+  export const FiMap: IconType;
+  export const FiTrash2: IconType;
+}
+
+declare module 'react-icons' {
+  import { ComponentType, SVGAttributes } from 'react';
+  
+  export interface IconBaseProps extends SVGAttributes<SVGElement> {
+    size?: string | number;
+    color?: string;
+    title?: string;
+  }
+
+  export type IconType = ComponentType<IconBaseProps>;
 }
